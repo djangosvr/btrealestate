592c9d7173efea97b2a258a43795501ea0d4616a
venkat <venkat.servepalli92@gmail.com>
2019-12-31 17:49:36 +0530

    first btre adding

---
+++ btre_project/btre/pages/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+'''# Register your models here.
+from .models import Listing
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id','name','email','hire_date')
+    list_display_links = ('id','name')
+    search_fields = ('name',)
+    list_per_page = 25
+admin.site.register(Listing,RealtorAdmin)
+'''
