632dbe92937106bdb71d61bdd8cded7b2f4021fc
djangosvr <servepallivenkat92@gmail.com>
2020-01-01 12:47:02 +0530

    btre first commit||Code base

---
+++ btrealestate/btre/realtors/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+'''
+def index(request):
+    return render(request,'realtors/realtors.html')
+def realtor(request):
+    return render(request,'realtors/realtor.html')
+def search(request):
+    return render(request,'realtors/search.html') 
+ '''
